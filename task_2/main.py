45da90b86e5cfa15cd49d3ead230d89cf8ea58a9
Alex Komendant <komendantaa@gmail.com>
2024-08-02 23:03:00 +0200

    feat (task_2)

---
+++ task_2/main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+
+def get_cats_info(path: str) -> list[dict[str, str]]:
+    PATH = Path(__file__).parent / path
+    with open(PATH, "r", encoding="utf-8") as file:
+        result = []
+        for line in file.readlines():
+            id, name, age = line.strip().split(",")
+            result.append({id, name, age})
+        return result
+
+
+cats_info = get_cats_info("cats.txt")
+print(cats_info)
